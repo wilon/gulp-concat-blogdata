20e26eb592e7261217e3f02ec871dccf7804395a
wangwl <wangwl@ifeng.com>
2019-05-08 15:27:56 +0800

    fine name / bug

--- index.js
+++ index.js
@@ -10,132 +10,133 @@ var Concat = require('concat-with-sourcemaps');
 // file can be a vinyl file object or a string
 // when a string it will construct a new one
 module.exports = function(file, opt) {
-  if (!file) {
-    throw new PluginError('gulp-concat', 'Missing file option for gulp-concat');
-  }
-  opt = opt || {};
+    if (!file) {
+        throw new PluginError('gulp-concat', 'Missing file option for gulp-concat');
+    }
+    opt = opt || {};
 
-  // to preserve existing |undefined| behaviour and to introduce |newLine: ""| for binaries
-  if (typeof opt.newLine !== 'string') {
-    opt.newLine = gutil.linefeed;
-  }
-
-  var isUsingSourceMaps = false;
-  var latestFile;
-  var latestMod;
-  var fileName;
-  var concat;
-
-  if (typeof file === 'string') {
-    fileName = file;
-  } else if (typeof file.path === 'string') {
-    fileName = path.basename(file.path);
-  } else {
-    throw new PluginError('gulp-concat', 'Missing path in file options for gulp-concat');
-  }
-
-  function bufferContents(file, enc, cb) {
-    // ignore empty files
-    if (file.isNull()) {
-      cb();
-      return;
+    // to preserve existing |undefined| behaviour and to introduce |newLine: ""| for binaries
+    if (typeof opt.newLine !== 'string') {
+        opt.newLine = gutil.linefeed;
     }
 
-    // we don't do streams (yet)
-    if (file.isStream()) {
-      this.emit('error', new PluginError('gulp-concat',  'Streaming not supported'));
-      cb();
-      return;
-    }
+    var isUsingSourceMaps = false;
+    var latestFile;
+    var latestMod;
+    var fileName;
+    var concat;
 
-    // enable sourcemap support for concat
-    // if a sourcemap initialized file comes in
-    if (file.sourceMap && isUsingSourceMaps === false) {
-      isUsingSourceMaps = true;
-    }
-
-    // set latest file if not already set,
-    // or if the current file was modified more recently.
-    if (!latestMod || file.stat && file.stat.mtime > latestMod) {
-      latestFile = file;
-      latestMod = file.stat && file.stat.mtime;
-    }
-
-    // construct concat instance
-    if (!concat) {
-      concat = new Concat(isUsingSourceMaps, fileName, opt.newLine);
-    }
-
-    // 文件名
-    var filename = file.history[0].replace(file.base, '').replace('.md', '');
-    var pre = new Buffer('tagname'+filename+'\n');
-
-    // add file to concat instance
-    concat.add(file.relative, pre + file.contents, file.sourceMap);
-    cb();
-  }
-
-  function endStream(cb) {
-    // no files passed in, no file goes out
-    if (!latestFile || !concat) {
-      cb();
-      return;
-    }
-
-    var joinedFile;
-
-    // if file opt was a file path
-    // clone everything from the latest file
     if (typeof file === 'string') {
-      joinedFile = latestFile.clone({contents: false});
-      joinedFile.path = path.join(latestFile.base, file);
+        fileName = file;
+    } else if (typeof file.path === 'string') {
+        fileName = path.basename(file.path);
     } else {
-      joinedFile = new File(file);
+        throw new PluginError('gulp-concat', 'Missing path in file options for gulp-concat');
     }
 
-    // 生成json
-    var allData = concat.content.toString('utf-8'),
-      dataTmp = {}, tagTmp = '',
-      data = [];
-    allData.split('\n').map(function(index, elem) {
-      var line = index.replace(/(^\s*)|(\s*$)/g, '');
-      if (/^tagname/.test(line)) {
-        tagTmp = line.replace(/^tagname/, '');
-        return;
-      }
-      // 空行
-      if (line == '') {
-        return;
-      }
-      // 语言标记行
-      if (/```\w+/.test(line)) {
-          dataTmp.lang = line.replace(/```/g, '');
-          return;
-      }
-      // 语言结束行，结束
-      if (line == '```' && dataTmp != []) {
-          data.push(dataTmp);
-          dataTmp = {};
-          return;
-      }
-      // 标题行
-      if (line.indexOf('###') == 0) {
-          dataTmp.tag = tagTmp;
-          dataTmp.name = line.replace(/^###/, '').replace(/(^\s*)|(\s*$)/g, '');
-          return;
-      }
-      dataTmp.des = (typeof dataTmp.des == 'undefined' ? '' : dataTmp.des) + index.replace(/^\ \ \ \ /, '') + '\n';
-    });
+    function bufferContents(file, enc, cb) {
+        // ignore empty files
+        if (file.isNull()) {
+            cb();
+            return;
+        }
 
-    joinedFile.contents = new Buffer(JSON.stringify(data));
+        // we don't do streams (yet)
+        if (file.isStream()) {
+            this.emit('error', new PluginError('gulp-concat', 'Streaming not supported'));
+            cb();
+            return;
+        }
 
-    if (concat.sourceMapping) {
-      joinedFile.sourceMap = JSON.parse(concat.sourceMap);
+        // enable sourcemap support for concat
+        // if a sourcemap initialized file comes in
+        if (file.sourceMap && isUsingSourceMaps === false) {
+            isUsingSourceMaps = true;
+        }
+
+        // set latest file if not already set,
+        // or if the current file was modified more recently.
+        if (!latestMod || file.stat && file.stat.mtime > latestMod) {
+            latestFile = file;
+            latestMod = file.stat && file.stat.mtime;
+        }
+
+        // construct concat instance
+        if (!concat) {
+            concat = new Concat(isUsingSourceMaps, fileName, opt.newLine);
+        }
+
+        // 文件名
+        var filename = file.history[0].replace(file.base + '/', '').replace('.md', '');
+        var pre = new Buffer('tagname' + filename + '\n');
+
+        // add file to concat instance
+        concat.add(file.relative, pre + file.contents, file.sourceMap);
+        cb();
     }
 
-    this.push(joinedFile);
-    cb();
-  }
+    function endStream(cb) {
+        // no files passed in, no file goes out
+        if (!latestFile || !concat) {
+            cb();
+            return;
+        }
 
-  return through.obj(bufferContents, endStream);
+        var joinedFile;
+
+        // if file opt was a file path
+        // clone everything from the latest file
+        if (typeof file === 'string') {
+            joinedFile = latestFile.clone({ contents: false });
+            joinedFile.path = path.join(latestFile.base, file);
+        } else {
+            joinedFile = new File(file);
+        }
+
+        // 生成json
+        var allData = concat.content.toString('utf-8'),
+            dataTmp = {},
+            tagTmp = '',
+            data = [];
+        allData.split('\n').map(function(index, elem) {
+            var line = index.replace(/(^\s*)|(\s*$)/g, '');
+            if (/^tagname/.test(line)) {
+                tagTmp = line.replace(/^tagname/, '');
+                return;
+            }
+            // 空行
+            if (line == '') {
+                return;
+            }
+            // 语言标记行
+            if (/```\w+/.test(line)) {
+                dataTmp.lang = line.replace(/```/g, '');
+                return;
+            }
+            // 语言结束行，结束
+            if (line == '```' && dataTmp != []) {
+                data.push(dataTmp);
+                dataTmp = {};
+                return;
+            }
+            // 标题行
+            if (line.indexOf('###') == 0) {
+                dataTmp.tag = tagTmp;
+                dataTmp.name = line.replace(/^###/, '').replace(/(^\s*)|(\s*$)/g, '');
+                return;
+            }
+            dataTmp.des = (typeof dataTmp.des == 'undefined' ? '' : dataTmp.des) + index.replace(/^\ \ \ \ /, '') + '\n';
+        });
+
+        joinedFile.contents = new Buffer(JSON.stringify(data));
+
+        if (concat.sourceMapping) {
+            joinedFile.sourceMap = JSON.parse(concat.sourceMap);
+        }
+
+        this.push(joinedFile);
+        cb();
+    }
+
+    return through.obj(bufferContents, endStream);
 };
